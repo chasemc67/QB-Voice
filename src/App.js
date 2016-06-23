d2eced0caeea3823b9bc0b1dbe96e3ff0672cd37
McCarty, Chase <Chase_McCarty@intuit.com>
2016-06-23 16:39:34 -0600

    Added GET code for intents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+    }
+
+    getIntents() {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://api.api.ai/v1/intents", true);
+            xhr.setRequestHeader("Authorization", "Bearer 56b6a6ab491c4850aaaecef72d1b6423");
+            xhr.setRequestHeader("accept", "application/json");
+            xhr.responseType = "json";
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    typeof xhr.response === "object" ? resolve(xhr.response) : resolve(JSON.parse(xhr.response));
+                } else {
+                    reject(`GET request failed with status = ${xhr.status} - ${xhr.statusText}`);
+                }
+            };
+            xhr.onerror = function() {
+                reject(`GET request failed with status = ${xhr.status} - ${xhr.statusText}`);
+            };
+            xhr.send();
+        });
+    }
+
+    /*
+    postIntents(payload) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", this.properties.responseUrl, true);
+            xhr.setRequestHeader("authorization", this.properties.apiKey);
+            xhr.setRequestHeader("accept", "application/json");
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.responseType = "json";
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    typeof xhr.response === "object" ? resolve(xhr.response) : resolve(JSON.parse(xhr.response));
+                } else if (xhr.status === 400) {
+                    reject(xhr.response.message);
+                } else {
+                    reject(`POST request failed with status = ${xhr.status} - ${xhr.statusText}`);
+                }
+            };
+            xhr.onerror = function() {
+                reject(`POST request failed with status = ${xhr.status} - ${xhr.statusText}`);
+            };
+            xhr.send(JSON.stringify(payload));
+        });
+    } */
+
+    render() {
+
+        this._promiseGetRequest().then((response) => {
+            console.log(response);
+        })
+
+
+        return (
+            <h1>QBVoice</h1>
+        );
+    }
 }
